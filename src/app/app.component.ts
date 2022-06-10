67bebf8fc2226f3400ba69df930cd33e24a8fecb
mhsnsrl <muhsinsrl@gmail.com>
2022-06-10 22:35:46 +0300

    Proje oluşturuldu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { LoremIpsum } from 'lorem-ipsum';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'harf';
+  lorem = new LoremIpsum({
+    sentencesPerParagraph: {
+      max: 8,
+      min: 4,
+    },
+    wordsPerSentence: {
+      max: 16,
+      min: 4,
+    },
+  });
+  randomText = this.lorem.generateSentences(1);
+
+  enteredText = '';
+
+  getInputValue(target: any) {
+    const value = target.value;
+    this.enteredText = value;
+    console.log(value);
+  }
+  compare(letter: string, enteredLetter: string) {
+    if (!enteredLetter) return 'pending';
+    return enteredLetter===letter ? "correct" : "incorrect"
+  }
 }
